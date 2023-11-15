19ad21b13f3ab40b7c148cc84da86835fe5b2ea0
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-11-15 14:52:49 +0100

    Replace APp with overview

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,17 @@
 import React from 'react'
-import './App.css'
-import App from './App'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 
 import 'preline'
+
+import Overview from './pages/overview'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/about"
-          element={
-            <>
-              <div className="text-center">
-                <h1 className="text-xl">About</h1>
-                <div>
-                  <Link to="/" className="text-purple-400 underline">
-                    Home
-                  </Link>
-                </div>
-              </div>
-            </>
-          }
-        />
+        <Route path="/" element={<Navigate to="/overview" />} />
+        <Route path="/overview" element={<Overview />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/overview.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react'
+
+function Overview(): ReactElement {
+  return <b>Overview</b>
+}
+
+export default Overview
