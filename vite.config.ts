c59e1df13bc0804f01456ba1fb05ed52c98c4ca1
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-11-21 19:40:54 +0100

    Add API server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://193.166.139.12',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
