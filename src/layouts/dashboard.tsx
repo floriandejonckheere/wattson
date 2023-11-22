ec76e3f11da07e7fb8b3c63a4c1da0f74269f4aa
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-11-22 22:26:33 +0100

    Add settings route and page

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -6,7 +6,8 @@ import {
   Cog6ToothIcon,
   HomeIcon,
   ChartBarIcon,
-  LightBulbIcon
+  LightBulbIcon,
+  UserIcon
 } from '@heroicons/react/24/solid'
 
 import { me } from '../api/users'
@@ -41,10 +42,10 @@ export default function Dashboard(): ReactElement {
         className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden top-0 start-0 bottom-0 z-[60] w-80 bg-sky-700 pt-4 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500"
       >
         <nav
-          className="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
+          className="hs-accordion-group p-6 w-full h-full flex flex-col flex-wrap"
           data-hs-accordion-always-open
         >
-          <ul className="space-y-4">
+          <ul className="space-y-4 h-full relative">
             <li>
               <NavLink
                 to="/overview"
@@ -108,11 +109,26 @@ export default function Dashboard(): ReactElement {
                 </NavLink>
               </li>
             )}
+            <li className="w-full absolute bottom-0 block">
+              <NavLink
+                to="/settings"
+                className={({ isActive }) =>
+                  `w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-white rounded-lg focus:outline-none focus:ring-1 focus:ring-gray-600 ${
+                    isActive
+                      ? 'bg-sky-900'
+                      : 'hover:bg-sky-900 hover:text-white-300'
+                  }`
+                }
+              >
+                <UserIcon className="h-5 w-5" />
+                Settings
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
 
-      <div className="p-5">
+      <div className="p-10">
         <h1 className="text-3xl font-bold mb-10">
           Good {moment}, {!isPending && !isError && data.username}!
         </h1>
