e306a82e11fd8c6ee46589f9b746e296abf140f1
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-11-24 11:39:33 +0100

    Add update password feature

--- src/api/users.tsx
+++ src/api/users.tsx
@@ -19,3 +19,33 @@ export function me() {
       return response.data
     })
 }
+
+export function changePassword(
+  newPassword: string,
+  confirmNewPassword: string
+) {
+  const token = localStorage.getItem('token')
+
+  if (!token) {
+    throw new AxiosError('Unauthorized')
+  }
+
+  return axios
+    .patch(
+      `/api/users/change_password`,
+      {
+        new_password: newPassword,
+        confirm_new_password: confirmNewPassword
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+          Authorization: `Bearer ${JSON.parse(token)}`
+        }
+      }
+    )
+    .then((response) => {
+      return response.data
+    })
+}
