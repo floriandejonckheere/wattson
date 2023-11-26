a5d3a3a0c9f7dff1390e430c3b32a47024bad6be
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-11-26 12:25:58 +0100

    Add location dropdown

---
+++ src/api/data.tsx
@@ -0,0 +1,12 @@
+export const Locations = {
+  ruissalo: {
+    name: 'Ruissalo',
+    lat: 60.426116973904996,
+    lon: 22.159310892022628
+  },
+  educity: {
+    name: 'EduCity',
+    lat: 60.44751,
+    lon: 22.2982
+  }
+}
--- src/pages/suggestions.tsx
+++ src/pages/suggestions.tsx
@@ -5,8 +5,11 @@ import { CloudIcon, SunIcon } from '@heroicons/react/24/outline'
 
 import TemperatureChart from '../components/charts/temperature'
 
+import { Locations } from '../api/data'
+
 export default function Suggestions(): ReactElement {
   const [activeTab, setActiveTab] = useState('today')
+  const [activeLocation, setActiveLocation] = useState(Locations.ruissalo)
 
   const weeklyForecast = [
     {
@@ -48,9 +51,51 @@ export default function Suggestions(): ReactElement {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 gap-16">
         <div className="flex flex-col gap-6 bg-white rounded-lg shadow p-7 dark:bg-slate-900">
           <div>
-            <h3 className="text-xl font-bold text-gray-600 dark:text-gray-200 mb-2">
-              Weather
-            </h3>
+            <div className="mb-6">
+              <div className="float-right">
+                <div className="hs-dropdown relative inline-flex">
+                  <button
+                    id="hs-dropdown-default"
+                    type="button"
+                    className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  >
+                    {activeLocation.name}
+                    <svg
+                      className="hs-dropdown-open:rotate-180 w-4 h-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="m6 9 6 6 6-6" />
+                    </svg>
+                  </button>
+
+                  <div
+                    className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+                    aria-labelledby="hs-dropdown-default"
+                  >
+                    {Object.values(Locations).map((location) => (
+                      <a
+                        className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700"
+                        href="#"
+                        onClick={() => setActiveLocation(location)}
+                      >
+                        {location.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <h3 className="text-xl font-bold text-gray-600 dark:text-gray-200 mb-2">
+                Weather
+              </h3>
+            </div>
             <div className="flex gap-3">
               <button
                 className={`py-1.5 px-3 rounded-full text-xs font-medium ${
