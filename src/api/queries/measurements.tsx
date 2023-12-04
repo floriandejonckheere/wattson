03460e89ee77ffa578f7aad00ec934d98936beec
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-04 16:09:56 +0100

    Show error message when no data is available

--- src/api/queries/measurements.tsx
+++ src/api/queries/measurements.tsx
@@ -3,17 +3,17 @@ import moment from 'moment'
 
 import { measurements } from '../measurements'
 
-import { Measurement, Sensor } from '../../types'
+import { Sensor } from '../../types'
 
 export const useMeasurements = (
   startTime: Date,
   endTime: Date,
   sensors: Sensor[]
-): { data: { string: Measurement }; isSuccess: boolean } => {
+) => {
   const start = moment(startTime).format()
   const end = moment(endTime).format()
 
-  const { isSuccess, data } = useQueries({
+  const { isSuccess, isError, data } = useQueries({
     queries: sensors.map((sensor) => ({
       queryKey: ['measurements', sensor.name, start, end],
       queryFn: () => measurements(sensor.device, sensor.sensor, start, end)
@@ -21,10 +21,19 @@ export const useMeasurements = (
     combine: (results) => {
       return {
         data: Object.assign({}, ...results.map((result) => result.data)),
-        isSuccess: results.every((result) => result.isSuccess)
+        isSuccess: results.every((result) => result.isSuccess),
+        isError: results.some((result) => result.isError)
       }
     }
   })
+
+  // API returns a HTTP 400 when there is no data
+  if (isError) {
+    return {
+      isSuccess: true,
+      data: {}
+    }
+  }
 
   return {
     isSuccess,
