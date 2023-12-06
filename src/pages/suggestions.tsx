ab64827d4c3be0d33b4fd0c126080adbca74d88a
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-06 21:56:31 +0100

    Add energy suggestions card

--- src/pages/suggestions.tsx
+++ src/pages/suggestions.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react'
 
 import EnergyAlerts from '../components/cards/energy_alerts'
+import EnergySuggestions from '../components/cards/energy_suggestions'
 import Weather from '../components/cards/weather'
 
 export default function Suggestions(): ReactElement {
@@ -11,6 +12,7 @@ export default function Suggestions(): ReactElement {
       <div className="grid grid-cols-1 xl:grid-cols-2 mt-6 gap-16">
         <EnergyAlerts />
         <Weather />
+        <EnergySuggestions />
       </div>
     </>
   )
