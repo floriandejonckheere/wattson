b8bb80760c57e965715a91db18974d693193159f
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-06 18:34:47 +0100

    Extract alerts to data

--- src/api/data.tsx
+++ src/api/data.tsx
@@ -1,4 +1,6 @@
-export const LOCATIONS = {
+import { Location, Sensor, Alert } from '../types'
+
+export const LOCATIONS: { ruissalo: Location; educity: Location } = {
   ruissalo: {
     name: 'Ruissalo',
     latitude: 60.426116973904996,
@@ -11,10 +13,41 @@ export const LOCATIONS = {
   }
 }
 
-export const SENSORS = {
+export const SENSORS: { heat_pump: Sensor } = {
   heat_pump: {
     name: 'Heat pump',
     device: 'Tyyssija_Heat_Pump',
     sensor: '131EE01Power'
   }
 }
+
+export const ALERTS: Alert[] = [
+  {
+    id: 1,
+    date: new Date(),
+    type: 'info',
+    color: 'blue',
+    title: 'New device',
+    message: 'A new device was detected: Smart Hub',
+    unread: true
+  },
+  {
+    id: 2,
+    date: new Date(),
+    type: 'warning',
+    color: 'yellow',
+    title: 'Energy usage',
+    message: 'Heat pump used 13% more energy last month',
+    unread: true
+  },
+  {
+    id: 3,
+    date: new Date(),
+    type: 'error',
+    color: 'red',
+    title: 'Unplug EV',
+    message:
+      'Your EV has been fully charged since 2 hours ago, unplug it from the shared charger to allow other residents to use it',
+    unread: false
+  }
+]
