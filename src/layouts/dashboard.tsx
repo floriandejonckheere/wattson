e920f36780196b6f1464c303cf9a6278bfdba308
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-09 10:49:39 +0100

    Use proper overflow in layout

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -36,16 +36,16 @@ export default function Dashboard(): ReactElement {
   }
 
   return (
-    <div className="flex flex-row w-full h-full bg-gray-50 dark:bg-slate-800 transition-all duration-500">
+    <div className="flex flex-row w-full h-full overflow-x-hidden bg-gray-50 dark:bg-slate-800 transition-all duration-500">
       <div
         id="application-sidebar-dark"
         className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden top-0 start-0 bottom-0 z-[60] w-80 bg-sky-700 pt-4 pb-6 overflow-y-auto dark:bg-sky-800 lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500"
       >
         <nav
-          className="hs-accordion-group p-6 w-full h-full flex flex-col flex-wrap"
+          className="hs-accordion-group p-6 w-full h-full"
           data-hs-accordion-always-open
         >
-          <ul className="space-y-4 h-full relative">
+          <ul className=" flex flex-col gap-4 h-full">
             <li>
               <NavLink
                 to="/overview"
@@ -61,7 +61,6 @@ export default function Dashboard(): ReactElement {
                 Overview
               </NavLink>
             </li>
-
             <li>
               <NavLink
                 to="/history"
@@ -109,7 +108,7 @@ export default function Dashboard(): ReactElement {
                 </NavLink>
               </li>
             )}
-            <li className="w-full absolute bottom-0 block">
+            <li className="w-full mt-auto">
               <NavLink
                 to="/settings"
                 className={({ isActive }) =>
@@ -128,7 +127,7 @@ export default function Dashboard(): ReactElement {
         </nav>
       </div>
 
-      <div className="p-10 w-full">
+      <div className="p-10 w-full overflow-y-scroll">
         <h1 className="text-3xl font-bold mb-10 dark:text-slate-300">
           Good {moment}
           {isSuccess && ', ' + data.username}!
