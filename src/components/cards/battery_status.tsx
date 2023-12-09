eedd7b75121f7c5450f9d9321e51af010daf95fe
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-12-09 16:43:35 +0100

    Add battery status queries

--- src/components/cards/battery_status.tsx
+++ src/components/cards/battery_status.tsx
@@ -1,5 +1,9 @@
 import { ReactElement } from 'react'
 
+import Spinner from '../spinner'
+
+import { useBatteryStatus } from '../../api/queries/battery_status'
+
 const Circle = ({ value }: { value: number }): ReactElement => (
   <div className="relative h-40 w-40">
     <svg
@@ -39,6 +43,12 @@ const Circle = ({ value }: { value: number }): ReactElement => (
 )
 
 export default function BatteryStatus(): ReactElement {
+  const { isSuccess, status } = useBatteryStatus()
+
+  if (!isSuccess) {
+    return <Spinner />
+  }
+
   return (
     <div className="flex flex-col gap-8 bg-white rounded-lg shadow p-7 dark:bg-slate-700 transition-all duration-500">
       <div>
@@ -55,7 +65,7 @@ export default function BatteryStatus(): ReactElement {
           Charging status
         </h4>
         <p className="flex justify-center mt-2 text-5xl text-sky-700 font-bold dark:text-gray-200">
-          <Circle value={25} />
+          <Circle value={status.charge} />
         </p>
       </div>
 
@@ -65,11 +75,8 @@ export default function BatteryStatus(): ReactElement {
             Battery capacity
           </h5>
           <p className="mt-1 text-3xl font-bold flex items-center dark:text-slate-300">
-            9.6&nbsp;
+            {(status.capacity / 1000).toFixed(1)}&nbsp;
             <span className="text-xl">kWh</span>
-          </p>
-          <p className="text-xs text-gray-400 dark:text-slate-400">
-            Since yesterday
           </p>
         </div>
         <div className="text-right">
@@ -77,11 +84,8 @@ export default function BatteryStatus(): ReactElement {
             Battery health
           </h5>
           <p className="mt-1 text-3xl font-bold flex justify-end items-center dark:text-slate-300">
-            100&nbsp;
+            {status.health}&nbsp;
             <span className="text-xl">%</span>
-          </p>
-          <p className="text-xs text-gray-400 dark:text-slate-400">
-            Since yesterday
           </p>
         </div>
       </div>
@@ -92,7 +96,7 @@ export default function BatteryStatus(): ReactElement {
             Total energy charged
           </h5>
           <p className="mt-1 text-3xl font-bold flex items-center dark:text-slate-300">
-            2 114&nbsp;
+            {Math.round(status.charged / 1000 ** 3)}&nbsp;
             <span className="text-xl">MJ</span>
           </p>
           <p className="text-xs text-gray-400 dark:text-slate-400">
@@ -104,7 +108,7 @@ export default function BatteryStatus(): ReactElement {
             Total energy discharged
           </h5>
           <p className="mt-1 text-3xl font-bold flex justify-end items-center dark:text-slate-300">
-            2 453&nbsp;
+            {Math.round(status.discharged / 1000 ** 3)}&nbsp;
             <span className="text-xl">MJ</span>
           </p>
           <p className="text-xs text-gray-400 dark:text-slate-400">
